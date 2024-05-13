bf1707720d51f07cbbe6484e77ad23ef46b8431a
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2024-05-13 11:02:59 -0500

    add gallery slide code

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+// anime slider (autoplay)
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function nextSlide() {
+    // Hide the current slide
+    slides[currentSlide].style.transform = 'translateX(-100%)';
+
+    // Update currentSlide index
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    // Display the next slide
+    slides[currentSlide].style.transform = 'translateX(0)';
+}
+
+// Autoplay the slider every 5 seconds
+setInterval(nextSlide, 5000);
+// end of gallery 
+
+
 const quizQuestions = [
     {
         question: "What genre of TV shows do you enjoy the most?",
